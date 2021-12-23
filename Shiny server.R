8732ed7340d882f7d44518577d66e44bc790e629
jimmy11299 <76819261+jimmy11299@users.noreply.github.com>
2021-12-23 19:08:18 +0800

    Update Shiny server.R

--- Shiny server.R
+++ Shiny server.R
@@ -2,35 +2,34 @@
 server<-function(input,output){
   #第一頁
   output$test <- renderDataTable(test_1,options = list(scrollX = TRUE))
+  test_1a<-reactive(test_1)
   output$download <- downloadHandler(
-    filename = function() {
-      paste0("房價資料表", ".tsv")
-    },
+    filename = function() {"房價資料表.csv"},
     content = function(file) {
-      vroom::vroom_write(data(), file)
-    }
+      write.csv(test_1a(), file)}
   )
   output$data_histogram_density_plot <-renderPlot(
     ggplot(as.data.frame(test_3),aes(x = V2))+  
-      geom_histogram(bins = 30, aes(y = ..density..), alpha = 0.5)+ 
-      geom_density()+ 
-      labs(x="房價", y="數量"))
+      geom_histogram(bins = 30, alpha = 0.5) +
+      labs(x="房價", y="數量",,caption="資料來源 : kaggle平台")+
+      theme(text=element_text(size=20,  family="wqy-microhei"))
+    )
   #第二頁
   output$table1<-renderTable(as.data.frame(c6))
   output$image4<-renderPlot(
     ggplot(df_pd2,aes(x = predict))+  
-      geom_histogram(bins = 30, aes(y = ..density..), alpha = 0.5)+ 
-      geom_density()+ 
-      labs(x="房價", y="數量")
+      geom_histogram(bins = 30, alpha = 0.5) +
+      labs(x="房價", y="數量",caption="資料來源 : kaggle平台")+
+      theme(text=element_text(size=20,  family="wqy-microhei"))
   )
   output$plot_h2o.RF<-renderPlot(h2o.varimp_plot(rf_f2, num_of_features = 10))
   output$table_h2o.RF<-renderDataTable(as.data.frame(h2o.varimp(rf_f2)))
   #第三頁
   output$image5<-renderPlot(
     ggplot(df_gbm_predict,aes(x = predict))+  
-      geom_histogram(bins = 30, aes(y = ..density..), alpha = 0.5)+ 
-      geom_density()+ 
-      labs(x="房價", y="數量")
+      geom_histogram(bins = 30, alpha = 0.5) +
+      labs(x="房價", y="數量",caption="資料來源 : kaggle平台")+
+      theme(text=element_text(size=20,  family="wqy-microhei"))
   )
   output$table2<-renderTable(b6)
   output$plot3<-renderPlot(h2o.varimp_plot(h2o.final, num_of_features = 10))
@@ -38,9 +37,9 @@ server<-function(input,output){
   #第四頁
   output$plot_dl1<-renderPlot(
     ggplot(df_pdl,aes(x = predict))+  
-      geom_histogram(bins = 30, aes(y = ..density..), alpha = 0.5)+ 
-      geom_density()+ 
-      labs(x="房價", y="數量")
+      geom_histogram(bins = 30, alpha = 0.5) +
+      labs(x="房價", y="數量",caption="資料來源 : kaggle平台")+
+      theme(text=element_text(size=20,  family="wqy-microhei"))
   )
   output$table_dl2<-renderTable(d6)
   output$plot_dl2<-renderPlot(h2o.varimp_plot(dl, num_of_features = 10))
@@ -48,4 +47,3 @@ server<-function(input,output){
 }
 
 shinyApp(ui = ui, server = server)
-
