ba9e19d9313fd0e48c3f2ed780c0967ab1065410
jimmy11299 <76819261+jimmy11299@users.noreply.github.com>
2021-12-28 20:50:13 +0800

    Update Shiny server.R

--- Shiny server.R
+++ Shiny server.R
@@ -8,39 +8,39 @@ server<-function(input,output){
     content = function(file) {
       write.csv(test_1a(), file)}
   )
-  output$data_histogram_density_plot <-renderPlot(
-    ggplot(as.data.frame(test_3),aes(x = V2))+  
+  output$data_histogram_density_plot <-renderPlotly(
+    ggplotly(ggplot(as.data.frame(test_3),aes(x = V2))+  
       geom_histogram(bins = 30, alpha = 0.5) +
       labs(x="房價", y="數量",,caption="資料來源 : kaggle平台")+
       theme(text=element_text(size=20,  family="wqy-microhei"))
-    )
+    ))
   #第二頁
   output$table1<-renderTable(as.data.frame(c6))
-  output$image4<-renderPlot(
-    ggplot(df_pd2,aes(x = predict))+  
+  output$image4<-renderPlotly(
+    ggplotly(ggplot(df_pd2,aes(x = predict))+  
       geom_histogram(bins = 30, alpha = 0.5) +
       labs(x="房價", y="數量",caption="資料來源 : kaggle平台")+
       theme(text=element_text(size=20,  family="wqy-microhei"))
-  )
+  ))
   output$plot_h2o.RF<-renderPlot(h2o.varimp_plot(rf_f2, num_of_features = 10))
   output$table_h2o.RF<-renderDataTable(as.data.frame(h2o.varimp(rf_f2)))
   #第三頁
-  output$image5<-renderPlot(
-    ggplot(df_gbm_predict,aes(x = predict))+  
+  output$image5<-renderPlotly(
+    ggplotly(ggplot(df_gbm_predict,aes(x = predict))+  
       geom_histogram(bins = 30, alpha = 0.5) +
       labs(x="房價", y="數量",caption="資料來源 : kaggle平台")+
       theme(text=element_text(size=20,  family="wqy-microhei"))
-  )
+  ))
   output$table2<-renderTable(b6)
   output$plot3<-renderPlot(h2o.varimp_plot(h2o.final, num_of_features = 10))
   output$table3<-renderDataTable(as.data.frame(h2o.varimp(h2o.final)))
   #第四頁
-  output$plot_dl1<-renderPlot(
-    ggplot(df_pdl,aes(x = predict))+  
+  output$plot_dl1<-renderPlotly(
+    ggplotly(ggplot(df_pdl,aes(x = predict))+  
       geom_histogram(bins = 30, alpha = 0.5) +
       labs(x="房價", y="數量",caption="資料來源 : kaggle平台")+
       theme(text=element_text(size=20,  family="wqy-microhei"))
-  )
+  ))
   output$table_dl2<-renderTable(d6)
   output$plot_dl2<-renderPlot(h2o.varimp_plot(dl, num_of_features = 10))
   output$table_dl1<-renderDataTable(as.data.frame(h2o.varimp(dl)))
